dbaeb03f6e925ed971c6f1579f65dda3c46f7fd7
AdamM18 <44504469+AdamM18@users.noreply.github.com>
2019-01-06 19:43:31 -0500

    Add files via upload

--- DotStar_Light_Blend.ino
+++ DotStar_Light_Blend.ino
@@ -1,13 +1,15 @@
 #include <Adafruit_DotStar.h>
 #include <SPI.h>
-#define numPatterns 3
+#define numPatterns 4
 
-#define MyBrightness 40
+#define MyBrightness 100
 #define CWoffset 0
-#define CCWoffset 0
-#define MyR 10
-#define MyG 10
-#define MyB 10
+#define CCWoffset 30
+#define CW2offset 60
+#define CCW2offset 90
+#define MyR 25
+#define MyG 15
+#define MyB 35
 
 float StepSize;
 float StepHold;
@@ -15,31 +17,31 @@ float StepHold;
 #define IDLED (numLED - 1)
 #define ButtonB 4
 byte ColorMode = 0, i = 0, state2 = 1, state1 = 1;
-float R = 0, G = 0, B = 0;
-float r = 0, g = 0, b = 0;
+float R = (MyBrightness/2), G = 0, B = 0;
+float r = (MyBrightness/2), g = 0, b = 0;
+byte MeteorDelay = random(50, 70);
 uint32_t MyColor;
 uint32_t PastMillis;
 
-#define dbDelay 110
+#define dbDelay 120
 bool ButtonState = false;
 uint32_t PrevBounceMillis;
 
 #define DATAPIN A6
 #define CLOCKPIN A7
 Adafruit_DotStar strip = Adafruit_DotStar(
-                           numLED, DATAPIN, CLOCKPIN, DOTSTAR_BRG);
+                           numLED, DATAPIN, CLOCKPIN, DOTSTAR_BGR);
 
 void setup() {
-  StepSize = MyBrightness*0.02;
-  strip.begin();
-  strip.show();
-  strip.setBrightness(255);
-  MyColor = strip.Color(MyR, MyG, MyB);
+  StepSize = MyBrightness * 0.001;
   (StepSize >= 1 ? StepHold = StepSize - 1 : StepHold = 0);
-  PrevBounceMillis = millis();
-  PastMillis = millis();
   pinMode(ButtonB, INPUT);
   digitalWrite(ButtonB, LOW);
+  strip.begin();
+  strip.show();
+  MyColor = strip.Color(MyR, MyG, MyB);
+  PrevBounceMillis = millis();
+  PastMillis = millis();
 }
 
 void loop() {
@@ -54,6 +56,9 @@ void loop() {
         break;
       case 3:
         CounterRunningLights();
+        break;
+      case 4:
+        MeteorRain(7u, 85, true);
         break;
       default:
         ConstLight();
@@ -71,43 +76,93 @@ void ConstBlend() {
 void RunningBlendRainbow() {
   FirstColor(StepSize);
   strip.setPixelColor(i, R, G, B);
-  if (i == IDLED) strip.show();
+  if (i == numLED / 2 || IDLED) strip.show();
   i++;
 }
 
 void CounterRunningLights() {
-  //  if (millis() - PastMillis > 80) {
-  //    PastMillis = millis();
-  byte CWa, CWb, CCWa, CCWb, CWi;
-
-  CWi = IDLED - i;
-  if (CWi < CWoffset) {
-    CWa = CWi + (IDLED - CWoffset + 1);
-  }
-  else CWa = CWi - CWoffset;
-  (CWa == IDLED ? CWb = 0 : CWb = CWa + 1);
-  FirstColor(StepSize);
-  strip.setPixelColor(CWa, R, G, B);
-  strip.setPixelColor(CWb, 0, 0, 0);
-
-  if (i > IDLED - CCWoffset) {
-    CCWa = i - (IDLED - CCWoffset + 1);
-  }
-  else CCWa = i + CCWoffset;
-  (CCWa == 0 ? CCWb = IDLED : CCWb = CCWa - 1);
-  SecondColor(StepSize);
-  strip.setPixelColor(CCWa, r, g, b);
-  strip.setPixelColor(CCWb, 0, 0, 0);
-
-  if (CCWa == CWb) {
-    strip.setPixelColor(CCWa, r, g, b);
-  }
-  if (CWa == CCWb) {
+    byte CWa, CWb, CCWa, CCWb, CWi = IDLED - i;;
+    byte CW2a, CW2b, CCW2a, CCW2b;
+    FirstColor(StepSize/2);
+    SecondColor(StepSize/2);
+    /* ===== Clockwise Light 1 ===== */
+    if (CWi < CWoffset) {
+      CWa = CWi + (IDLED - CWoffset + 1);
+    }
+    else CWa = CWi - CWoffset;
+    (CWa == IDLED ? CWb = 0 : CWb = CWa + 1);
     strip.setPixelColor(CWa, R, G, B);
+    strip.setPixelColor(CWb, 0, 0, 0);
+
+    /* ===== Clockwise Light 2 ===== */
+    if (CWi < CW2offset) {
+      CW2a = CWi + (IDLED - CW2offset + 1);
+    }
+    else CW2a = CWi - CW2offset;
+    (CW2a == IDLED ? CW2b = 0 : CW2b = CW2a + 1);
+    strip.setPixelColor(CW2a, R, G, B);
+    strip.setPixelColor(CW2b, 0, 0, 0);
+
+    /* ===== Counter Clockwise Light 1 ===== */
+    if (i > IDLED - CCWoffset) {
+      CCWa = i - (IDLED - CCWoffset + 1);
+    }
+    else CCWa = i + CCWoffset;
+    (CCWa == 0 ? CCWb = IDLED : CCWb = CCWa - 1);
+    strip.setPixelColor(CCWa, r, g, b);
+    strip.setPixelColor(CCWb, 0, 0, 0);
+
+    /* ===== Counter Clockwise Light 2 ===== */
+    if (i > IDLED - CCW2offset) {
+      CCW2a = i - (IDLED - CCW2offset + 1);
+    }
+    else CCW2a = i + CCW2offset;
+    (CCW2a == 0 ? CCW2b = IDLED : CCW2b = CCW2a - 1);
+    strip.setPixelColor(CCW2a, r, g, b);
+    strip.setPixelColor(CCW2b, 0, 0, 0);
+
+    /* ===== Overlapping Lights ===== */
+    if (CCWb == CWa || CCW2b == CWa)
+      strip.setPixelColor(CWa, R, G, B);
+    if (CCW2b == CW2a || CCWb == CW2a)
+      strip.setPixelColor(CW2a, R, G, B);
+    /**/
+    i++;
+    strip.show();
+}
+
+void MeteorRain(byte meteorSize, byte meteorTrailDecay, bool meteorRandomDecay) {
+  for (i = 0; i < numLED + (meteorSize);) {
+    if (millis() - PastMillis > MeteorDelay) {
+      MeteorDelay = random(50, 70);
+      PastMillis = millis();
+      for (byte j = 0; j < numLED; j++) {
+        if ( (!meteorRandomDecay) || (random(11) > 8) ) {
+          fadeToBlack(j, meteorTrailDecay);
+        }
+      }
+
+      for (byte j = 0; j < meteorSize; j++) {
+        if ( ( i - j < numLED) && (i - j >= 0) ) {
+          strip.setPixelColor(i - j, MyColor);
+        }
+      }
+
+      strip.show();
+      i++;
+    }
   }
-  i++;
-  //  }
-  strip.show();
+}
+void fadeToBlack(byte ledNo, byte fadeValue) {
+  uint32_t oldColor = strip.getPixelColor(ledNo);
+  byte MeteorR = (oldColor & 0x00ff0000UL) >> 16;
+  byte MeteorG = (oldColor & 0x0000ff00UL) >> 8;
+  byte MeteorB = (oldColor & 0x000000ffUL);
+  
+  MeteorR = (MeteorR <= (MyR/5)) ? MeteorR = 0 : (MeteorR * fadeValue / 256);
+  MeteorG = (MeteorG <= (MyG/5)) ? MeteorG = 0 : (MeteorG * fadeValue / 256);
+  MeteorB = (MeteorB <= (MyB/5)) ? MeteorB = 0 : (MeteorB * fadeValue / 256);
+  strip.setPixelColor(ledNo, MeteorR, MeteorG, MeteorB);
 }
 
 void ConstLight() {
